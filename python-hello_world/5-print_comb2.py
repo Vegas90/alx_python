a8f1389ebbf72264fc284f2bb7ed90b74a8e7fa4
Vegas90 <amosmwoni@gmail.com>
2023-07-18 22:47:24 +0300

    m

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,6 +1,9 @@
-for i in range(100):
-     if i< 99:
-          print("{:02d},".format(i), end=" ")
+#for i in range(100):
+   #  if i< 99:
+          #print("{:02d},".format(i), end=" ")
           
-     else:
-      print("{:02d}".format(i), end=" ")
+    # else:
+      #print("{:02d}".format(i), end=" ")
+      
+numbers = ", ".join(["{:02d}".format(i) for i in range(100)])
+print(numbers)
