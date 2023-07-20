0dce6c5784149f9bb76f05c91d4d46577d5b6976
Vegas90 <amosmwoni@gmail.com>
2023-07-20 22:19:12 +0300

    m

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
+amos= "False"
+vegas= "True"
 def is_prime(n):
     if n > 1:
         for i in range(2, int(n/2)+1):
              if (n % i) == 0:
-                    print( "False")
+                    return amos
                     break
         else:
-             print("True")
+             return vegas
 # If the number is less than 1, its also not a prime number.
     else:
-	     return print("False")
-     
+	     return amos
+  
+print(is_prime(0))     
      
