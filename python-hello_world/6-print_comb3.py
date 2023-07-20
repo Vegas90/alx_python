15e1e0b0160a1509d14971ad3f3b8762c94f7925
Vegas90 <amosmwoni@gmail.com>
2023-07-20 10:50:36 +0300

    m

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,5 +1,4 @@
-for i in range(10):
-    for j in range(i+1, 10):
-             vegas = sorted([i, j])
-print("{:02d}, {:02d}".format(vegas[0], vegas[1]))
-            
+for i in range(0, 10):
+    for j in range(0, 10):
+        print("{}, ".format(i) + "{}".format(j), end="")
+print()
