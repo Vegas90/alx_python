082503873eb38fc0bdff061f3884462888cde67e
Vegas90 <amosmwoni@gmail.com>
2023-07-20 12:18:25 +0300

    done

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -2,15 +2,6 @@
 def add(a, b):
     return a + b
 
-add = __import__('0-sum').add
-
-print(add(1, 2))     # Output: 3
-print(add(98, 0))    # Output: 98
-print(add(100, -2))  # Output: 98
-
-#!/usr/bin/env python3
-add = __import__('0-sum').add
-
 print(add(1, 2))
 print(add(98, 0))
-print(add(100, -2))
+print(add(100, -2))     
