78b692b0135bf549d2ba266c0bcf70be3dbf20aa
Vegas90 <amosmwoni@gmail.com>
2023-07-25 23:07:13 +0300

    m

--- python-import_modules/2-variable_load.py
+++ python-import_modules/2-variable_load.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from variable_load_2 import a
+#if __name__ == "__main__":
+    #from variable_load_2 import a
+    
+import variable_load_2
+print(variable_load_2.a)
