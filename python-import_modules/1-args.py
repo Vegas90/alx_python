710b2f1d1a9a5e6253ff8e15d105c6a34dea293a
Vegas90 <amosmwoni@gmail.com>
2023-07-25 22:49:03 +0300

    m

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,25 +1,11 @@
 import sys
-
-if __name__ == '__main__':
-    # Get the number of arguments
-    num_args = len(sys.argv) - 1  # The first element in sys.argv is the script name
-
-    # Print the number of arguments
-    print(f"Number of argument(s): {num_args}", end="")
-
-    # Print "argument" or "arguments" based on the number of arguments
-    if num_args == 1:
-        print(" followed by argument:", end="")
-    else:
-        print(" followed by arguments:", end="")
-
-    # Print a newline character after the header
-    print()
-
-    # Print each argument and its position
-    for i, arg in enumerate(sys.argv[1:], start=1):
-        print(f"{i}: {arg}")
-
-    # If no arguments were passed, print a dot
-    if num_args == 0:
-        print(".")
+def main():
+  # Get the number of arguments
+  num_args = len(sys.argv)
+  # Print the number of arguments
+  print("Number of arguments:", num_args)
+  # Print the arguments
+  for i in range(1, num_args + 1):
+    print("Argument ", i, ":", sys.argv[i - 1])
+if __name__ == "__main__":
+  main()
