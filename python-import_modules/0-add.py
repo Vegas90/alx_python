e693ac1c7eb040c0034ae8b4bb40f1e9eea82c3d
Vegas90 <amosmwoni@gmail.com>
2023-07-25 12:41:02 +0300

    m

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-add = __import__('0-add').add
-a = 1
-b = 2
-
 def add(a,b):
-    return a+b
+    a = 1
+    b = 2
 
-result = add(a,b)
+    result = add(a,b)
+    return result
 
 print("{} + {} = {}".format(a,b,result) )
 
