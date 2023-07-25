c73b4c45c8db5d7504ac1f7bb5281bded031bf3f
Vegas90 <amosmwoni@gmail.com>
2023-07-25 22:53:35 +0300

    m

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -5,6 +5,9 @@ def main():
   # Print the number of arguments
   print("Number of arguments:", num_args)
   # Print the arguments
+  if num_args == 0:
+      print("0 arguments.")
+  
   for i in range(1, num_args + 1):
     print(i, ":", sys.argv[i - 1])
 if __name__ == "__main__":
