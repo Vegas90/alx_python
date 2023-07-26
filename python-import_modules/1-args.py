ddc12dd5631c386843b1dba397b992cce19e4617
Vegas90 <amosmwoni@gmail.com>
2023-07-26 13:49:36 +0300

    m

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -10,8 +10,8 @@ def main():
     print("{} arguments:".format(num_args))
   
    # Print the arguments
-        for i in range(1, num_args):
-            print(i, ":", sys.argv[i])
+    for i in range(1, num_args):
+          print(i, ":", sys.argv[i])
 
 
 
