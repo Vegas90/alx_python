f7df42afd9d6bae495e33fd332811b81938d9388
Vegas90 <amosmwoni@gmail.com>
2023-07-26 13:36:48 +0300

    m

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,11 +1,19 @@
-import sys
+#import sys
 def main():
-  # Get the number of arguments
-  num_args = len(sys.argv)
-  # Print the number of arguments
-  print("Number of arguments:", num_args)
-  # Print the arguments 
-  for i in range(1, num_args + 1):
-    print(i, ":", sys.argv[i - 1])
+  num_args=len(sys.argv)
+  
+if num_args== 1:
+  print( num_args,"argument.")
+
+else:
+ print("{} arguments:"format(num_args))
+  
+  #Print the arguments 
+for i in range(1, num_args + 1):
+print(i, ":", sys.argv[i - 1])
+
+
+
 if __name__ == "__main__":
   main()
+
