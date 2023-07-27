284d6a151f0527eb972af1ecadd89c390faeed12
Vegas90 <amosmwoni@gmail.com>
2023-07-28 01:38:18 +0300

    m

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
-def multiple_returns(sentence):  
-    
-    print("Length: {:d} - First character: {}".format(len(sentence), sentence[0]))   
+def multiple_returns(sentence):
+    length = len(sentence)
+    first_char = sentence[0] if sentence else None
+    return length, first_char
+
+if __name__ == "__main__":
+    length, first_char = multiple_returns(sentence)
+    print("Length: {:d} - First character: {}".format(length, first_char))
