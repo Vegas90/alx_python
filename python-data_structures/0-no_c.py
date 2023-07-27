b63d21eb27a16fda4b0f54f7b200382a4879c5b7
Vegas90 <amosmwoni@gmail.com>
2023-07-27 23:43:30 +0300

    m

---
+++ python-data_structures/0-no_c.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+no_c = __import__('0-no_c').no_c
+
+result = ""
+def no_c(my_string):
+
+for char in my_string:
+ if char != 'c' and char !=C:
+       
+    result +=char
+return result    
+       
+#no_c(my_string)
+
+
+
+
+
+
+
+
+
+#if __name__ == "__main__":
+#  main()
