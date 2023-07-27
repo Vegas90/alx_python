c36420a91295fe17644b4c5070fed99973b2733b
Vegas90 <amosmwoni@gmail.com>
2023-07-27 23:44:29 +0300

    m

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
-no_c = __import__('0-no_c').no_c
+#no_c = __import__('0-no_c').no_c
 
 result = ""
 def no_c(my_string):
 
-for char in my_string:
- if char != 'c' and char !=C:
+    for char in my_string:
+         if char != 'c' and char !=C:
        
-    result +=char
-return result    
+             result +=char
+    return result    
        
 #no_c(my_string)
 
