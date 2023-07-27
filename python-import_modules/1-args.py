5ed6ce61e544cd7f2b329d5d3d031d0d7cd5e574
Vegas90 <amosmwoni@gmail.com>
2023-07-27 10:24:30 +0300

    m

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -2,8 +2,8 @@ import sys
 center_args=0
 def main():
   num_args=len(sys.argv)-1
-  if len(sys.argv) <= 1:
-    print( num_args,"arguments.")
+  if len(sys.argv) < 2:
+    print( num_args,"argument:")
   
   else:
     print("{} arguments:".format(num_args))
