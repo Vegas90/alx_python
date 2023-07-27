bb6bcd289d667ef064cf2eb65eb24c985266647b
Vegas90 <amosmwoni@gmail.com>
2023-07-27 10:17:26 +0300

    m

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -2,14 +2,13 @@ import sys
 center_args=0
 def main():
   num_args=len(sys.argv)-1
-  
   if len(sys.argv) == 1:
     print( num_args,"argument:")
   
   else:
     print("{} arguments:".format(num_args))
    # Print the arguments
-    for i in range(1, num_args - 1):
+    for i in range(0, num_args):
       print(i, ":", sys.argv[i+1])
 
 
