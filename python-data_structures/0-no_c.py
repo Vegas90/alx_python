b6e290d9254002df0a0a7b9cec17e21b9ec369db
Vegas90 <amosmwoni@gmail.com>
2023-07-28 00:39:25 +0300

    m'

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -3,10 +3,14 @@
 
 
 def no_c(my_string):
+    # Initialize an empty string to store the result
     result = ""
+     # Loop through each character in the input string
     for char in my_string:
+         # Check if the character is not 'c' and not 'C'
          if char != 'c' and char !='C':
-       
+                   # Append the character to the result string
+
              result +=char
     return result    
        
---
+++ python-data_structures/1-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+#print_matrix_integer = __import__('1-print_matrix_integer').print_matrix_integer
+a=[1,2,3]
+b=[4,5,6]
+c=[7,8,9]
+
+print(a)
+print(b)
+print(c)
