68ca840ebb97db4b22e6f8dba9997735745c97ae
Vegas90 <amosmwoni@gmail.com>
2023-07-28 00:55:16 +0300

    m

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 #print_matrix_integer = __import__('1-print_matrix_integer').print_matrix_integer
-a=[1,2,3]
-b=[4,5,6]
-c=[7,8,9]
-
-print("{a}".format(a=a), end='\n')
+def print_matrix_integer(matrix=[[]]):
+   for row in matrix:
+        for element in row:
+            print("{:d}".format(element), end=" ")
+        print() 
+        # Add a newline after each row 
+    
+print_matrix_integer(matrix)
+#print("{a}".format(a=a), end='\n')
 #print("{}".format(b), end='\n')
 #print("{}".format(c), end='\n')
