4adb6dbd3d5ec8b6f3bc9c2a883905f933003dd0
Vegas90 <amosmwoni@gmail.com>
2023-07-31 23:06:50 +0300

    m

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+square_matrix_simple = __import__('0-square_matrix_simple').square_matrix_simple
+
 matrix:[[1,2,3], [4,5,6], [7,8,9]]
 
 new_matrix=print(list(map(lambda x:[x**2 for i in x], matrix)))
