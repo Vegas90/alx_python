7c3a19c2d0ec177df48ad482a3ec53e00f6e8507
Vegas90 <amosmwoni@gmail.com>
2023-07-31 23:03:15 +0300

    m

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python3
-#no_c = __import__('0-no_c').no_c
-
-
 def no_c(my_string):
     # Initialize an empty string to store the result
     result = ""
@@ -13,10 +10,6 @@ def no_c(my_string):
 
              result +=char
     return result    
-       
-    #no_c(my_string)
-#if __name__ == "__main__":
-   # main()
 
 
 
