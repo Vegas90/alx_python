735871c6212ad208e6fb822b9913c73b22e01f79
Vegas90 <amosmwoni@gmail.com>
2023-07-31 23:29:41 +0300

    m

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -4,8 +4,8 @@ square_matrix_simple = __import__('0-square_matrix_simple').square_matrix_simple
 def square_matrix_sample(matrix=[]):
 #new_matrix=0   
  # Create a new matrix using list comprehension
-new_matrix = [[x ** 2 for x in row] for row in matrix]
+    new_matrix = [[x ** 2 for x in row] for row in matrix]
 #new_matrix=print(list(map(lambda x:[i**2 for i in x], matrix)))
-return new_matrix
+    return new_matrix
 
 
