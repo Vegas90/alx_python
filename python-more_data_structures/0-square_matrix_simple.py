dbaf75948e1a07e48775a2c1b2293a9e1dd35d08
Vegas90 <amosmwoni@gmail.com>
2023-07-31 23:17:12 +0300

    m

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -3,7 +3,7 @@
 matrix=0
 matrix:[[1,2,3], [4,5,6], [7,8,9]]
 
-new_matrix=print(list(map(lambda x:[x**2 for i in x], matrix)))
+new_matrix=print(list(map(lambda x:[i**2 for i in x], matrix)))
 
 print (new_matrix)
 print (matrix)
