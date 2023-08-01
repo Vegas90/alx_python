888a817ec98d973d3038e1a50b7a0150e8391df4
Vegas90 <amosmwoni@gmail.com>
2023-08-01 13:12:04 +0300

    m

---
+++ python-more_data_structures/2-update_dictionary.py
@@ -0,0 +1,10 @@
+def update_dictionary(a_dictionary, key, value):
+        
+    if a_dictionary.get(key):
+         a_dictionary[key] = value
+        
+    else:
+        a_dictionary[key] = value
+        
+    return a_dictionary
+    
