bc2c4e07f35d84accd377dd115042c7eb741213e
Vegas90 <amosmwoni@gmail.com>
2023-08-01 13:47:31 +0300

    m

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -1,9 +1,9 @@
 def best_score(a_dictionary):
     best_key= None
-    best_score = int
-    for key, value in a_dictionary.item:
-      if isinstance(value, int) and value > best_score:
+    largest_value= int
+    for key, value in a_dictionary.items():
+      if value > largest_value:
            best_key = key
-           best_score = value
+           largest_value = value
              
     print(best_key)
