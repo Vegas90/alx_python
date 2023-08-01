94687606d825f073859a466ab333f622e63f900f
Vegas90 <amosmwoni@gmail.com>
2023-08-01 12:01:36 +0300

    M

--- python-more_data_structures/1-common_elements.py
+++ python-more_data_structures/1-common_elements.py
@@ -3,6 +3,7 @@ def common_elements(set_1, set_2):
     #iterate through the set 1 and for each element we check if its present in set 2 
     for item in set_1:
         if item in set_2:
+            #common items added in the new set by add.(item)
             common_elements_set.add(item)
     return common_elements_set
   
