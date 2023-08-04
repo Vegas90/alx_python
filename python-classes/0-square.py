c489edcf42b5455020007238cebbfcd6673b532d
Vegas90 <amosmwoni@gmail.com>
2023-08-04 16:10:54 +0300

    m

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,30 +1,5 @@
-class MyClass:
-    """
-    This class represents an example class with a private instance attribute.
-    """
-
-    def __init__(self):
-        """
-        Constructor for MyClass.
-
-        This method initializes the private attribute __private_attr with the value 42.
-        """
-        self.__private_attr = 42  # Private instance attribute
-
-    def get_private_attr(self):
-        """
-        Get the value of the private attribute.
-
-        Returns:
-            int: The value of the private attribute.
-        """
-        return self.__private_attr
-
-    def set_private_attr(self, value):
-        """
-        Set the value of the private attribute.
-
-        Args:
-            value (int): The value to set the private attribute to.
-        """
-        self.__private_attr = value
+#python3 -c 'print(__import__("my_module").__doc__)
+class Square:
+#python3 -c 'print(__import__("my_module").MyClass.__doc__)'
+    def __init__(self,size):
+        size = self.__size
