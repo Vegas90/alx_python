90cde46b306169332b5fb6e4945b7e3c6ea28ba1
Vegas90 <amosmwoni@gmail.com>
2023-08-04 16:48:49 +0300

    m

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -15,7 +15,7 @@ Style Guide`_. Docstrings may extend over multiple lines. Sections are created
 with a section header and a colon followed by a block of indented text.
 
 """
-def __init__(self,size):
+def __dir__(self,size):
         self.__size = size
         
     
