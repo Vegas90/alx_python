f951b3966891d3567f256ae4906439ce25322f87
Vegas90 <amosmwoni@gmail.com>
2023-08-04 16:12:49 +0300

    m

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -6,6 +6,12 @@ with a section header and a colon followed by a block of indented text.
 
 """
 class Square:
-#python3 -c 'print(__import__("my_module").MyClass.__doc__)'
-    def __init__(self,size):
+"""Example Google style docstrings.
+
+This module demonstrates documentation as specified by the `Google Python
+Style Guide`_. Docstrings may extend over multiple lines. Sections are created
+with a section header and a colon followed by a block of indented text.
+
+"""
+def __init__(self,size):
         size = self.__size
