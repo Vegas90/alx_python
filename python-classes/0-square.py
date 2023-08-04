d61b0155c8c2f789c77f0742c02753a404cf13b4
Vegas90 <amosmwoni@gmail.com>
2023-08-04 16:58:47 +0300

    m

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,20 +1,14 @@
-"""Example Google style docstrings.
-
-This module demonstrates documentation as specified by the `Google Python
-Style Guide`_. Docstrings may extend over multiple lines. Sections are created
-with a section header and a colon followed by a block of indented text.
+"""
+declare class
 
 """
 class Square:
-    """
-Example Google style docstrings.
 
-This module demonstrates documentation as specified by the `Google Python
-Style Guide`_. Docstrings may extend over multiple lines. Sections are created
-with a section header and a colon followed by a block of indented text.
+    """
+adds up
 
 """
-def __dir__(self,size):
+def __innit__(self,size):
         self.__size = size
         
     
