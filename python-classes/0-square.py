f9c9d458086ec7b67089bd8b0f1706efd788c33c
Vegas90 <amosmwoni@gmail.com>
2023-08-05 21:16:55 +0300

    m

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -5,11 +5,10 @@ module with a class
 """
 
 class Square:
-
     """
 adds up
 
 """
-def __init__(self, size):
+    def __init__(self, size):
         self.__size = size
-        """ private attribute"""
+    """ private attribute"""
