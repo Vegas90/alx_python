50c4b8758483df9d4baa7f4900b563ddd897139e
Vegas90 <amosmwoni@gmail.com>
2023-08-05 14:44:30 +0300

    m

---
+++ python-classes/1-square.py
@@ -0,0 +1,18 @@
+"""
+declare class
+
+"""
+
+class Square:
+
+    """
+adds up
+
+"""
+def __innit__(self,size = 0):
+       if type(size) is not int:
+           raise TypeError("size must be an integer")
+       elif size < 0:
+           raise ValueError("size must be >=0")
+       else:
+           self.__size=size
