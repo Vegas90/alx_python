a5058643af3194923f38c244a3623ee4f5859cc7
Vegas90 <amosmwoni@gmail.com>
2023-08-05 21:17:51 +0300

    m

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -9,7 +9,7 @@ class Square:
 adds up
 
 """
-def __init__(self,size = 0):
+    def __init__(self,size = 0):
        if type(size) is not int:
            raise TypeError("size must be an integer")
        elif size < 0:
