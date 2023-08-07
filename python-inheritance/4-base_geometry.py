da1defbcaa8df09e890067ff9411ae5cc5910b12
Vegas90 <amosmwoni@gmail.com>
2023-08-07 22:06:35 +0300

    m

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,12 +1,12 @@
 """Class"""
 class BaseGeometry:
-    """class"""
+    """class
     def __dir__(self):
         attributes = super().__dir__()
         new_attribute_list = [
             item for item in attributes if item != "__innit subclass"]
         return new_attribute_list    
-        
+     """   
         
         
     def area(self):
