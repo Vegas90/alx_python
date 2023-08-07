41829486718f640fca338e28754ead5d5c5bdb82
Vegas90 <amosmwoni@gmail.com>
2023-08-07 23:29:19 +0300

    m

---
+++ python-inheritance/8-square.py
@@ -0,0 +1,53 @@
+"""Class"""
+class BaseGeometry():
+    """class
+    
+     """
+    def __dir__(self):
+        attributes = super().__dir__()
+        new_attribute_list =[
+            item for item in attributes if item != "__init_subclass__"]
+        return new_attribute_list
+     
+            
+    def area(self):
+        """class"""
+        raise Exception ("area() is not implemented")
+    """class"""
+    #@area.setter
+        #"""class"""
+    def integer_validator(self, name, value):
+        """class"""
+        if not isinstance(value, int):
+            """class"""
+            raise TypeError(f"{name} must be an integer")
+        elif value == 0 :
+            """Class"""
+            raise ValueError(f"{name} must be greater than 0")
+        else:
+            """Class"""
+            return value
+        
+        
+class Rectangle(BaseGeometry):
+    """class"""
+    def __init__(self, width, height):
+        """class"""
+        self.__width = super().integer_validator("width",width)
+        self.__height = super().integer_validator("height", height)
+        
+    def area(self):
+        return self.__width * self.__height
+    
+    def print(self):
+        print(self.area())
+    
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+class Square(Rectangle):
+    def __init__(self,size):
+        self.__size = super().integer_validator("size",size)
+        
+    def area(self):
+        return self.__size ** 2
