90524845619a193ef3730030e47be7cba7104418
Vegas90 <amosmwoni@gmail.com>
2023-08-10 00:37:31 +0300

    m

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -102,4 +102,6 @@ class Rectangle(Base):
         for _ in range(self.__height):
             print("#" * self.__width)
             
-        
+    def __str__(self):
+        return(f"Rectangle {self.id} {self.__x}/{self.__y} - {self.__width/self.__height})")
+    
