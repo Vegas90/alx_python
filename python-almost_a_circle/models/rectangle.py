425babae8ecc77d2438835f1363738707bf0b1d1
Vegas90 <amosmwoni@gmail.com>
2023-08-10 00:16:41 +0300

    m

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -28,12 +28,13 @@ class Rectangle(Base):
     @width.setter
     def width(self,width):
         """setter width"""
-        if isinstance(width, (int)) or str(float(width)).isdigit():
-            raise TypeError(f"{width} must be an integer")
+        value =int(width)    
+        if not isinstance(value, int):
+            raise TypeError(f"{value} must be an integer")
         elif int(width) <= 0:
-            raise ValueError(f"{width} must be > 0")
+            raise ValueError(f"{value} must be > 0")
         else:
-            self.__width= width
+            self.__width= value
     
        
     @property
