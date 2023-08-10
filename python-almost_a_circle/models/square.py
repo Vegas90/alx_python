7f4be725f106d9d6a6fd2985c80f9c9b3d7cf388
Vegas90 <amosmwoni@gmail.com>
2023-08-10 23:03:52 +0300

    m

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -2,12 +2,15 @@
 
 """class"""
 class Square(Rectangle):
-    """frp, rectangle"""
+    """
+    frp, rectangle"""
     def __init__(self, size, x=0, y=0, id=None):
-         """initialize the inherited values"""
+         """
+         initialize the inherited values"""
          super().__init__(size,x=0, y=0, id = None)
          
     def __str__(self):
-        """rectangle"""
+        """
+        rectangle"""
         return (f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}")
        
