9f75daf138f9d5247a415caeebd58539e92be914
Vegas90 <amosmwoni@gmail.com>
2023-08-11 19:14:51 +0300

    M

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -10,16 +10,6 @@ response = requests.get(url)
 """
 commented
 """
-if response.status_code == 200:
-    """
-    commit my code
-    """
-    #content = response.json()
-    print("Body response:")
-    print("type:", type(response))
-    print("content:", response)
-else:
-    """
-    commit 
-    """
-    print(f"Request failed with status code: {response.status_code}")
+print("Body response:")
+print("type:", type(response.text))
+print("content:", response.text)
