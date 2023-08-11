3d985e90aa0a0c446cb89acaa78f47a5e072218f
Vegas90 <amosmwoni@gmail.com>
2023-08-11 16:46:12 +0300

    m

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -8,7 +8,11 @@ url= "https://alu-intranet.hbtn.io/status"
 """_summary_
 """
 response = requests.get(url)
+""""summary"""
 content = response.json()
+""""summary"""
 print("Body response:")
+""""summary"""
 print("\t- type:", type(content))
+""""summary"""
 print("\t- content:", content)
