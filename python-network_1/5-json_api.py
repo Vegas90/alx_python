f0c470c1153502d4154327a748bcae8630bec1e6
Vegas90 <amosmwoni@gmail.com>
2023-08-11 23:34:06 +0300

    M

--- python-network_1/5-json_api.py
+++ python-network_1/5-json_api.py
@@ -5,19 +5,20 @@ url= "http://0.0.0.0:5000/search_user"
 
 if len(sys.argv) > 1:
     q = sys.argv[1]
-    
-    data = {'q': q}
+else:
+    q=""
+data = {'q': q}
     
 response = requests.get(url, data=data)
 try:
     colar= response.json()
     if colar:
-        print('[', colar['id'], '] ', colar['name'])
+        print(f"[{colar['id']}] {colar['name']}")
     else:
         print("No result")
     
 except ValueError:
     print("Not a valid JSON")
 
-
+q= 2
 
