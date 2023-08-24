745689e1ee3efd360f9ca531655d90050e6871f5
Vegas90 <amosmwoni@gmail.com>
2023-08-24 15:41:36 +0300

    M

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -6,7 +6,8 @@ db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], 
 cursor= db.cursor()
 
 #Execute the query to list states in ascending order by id
-query = "SELECT * FROM states WHERE states.name LIKE 'N%' ORDER BY states.id ASC"
+#BINARY ensures the comparison is case sensitive
+query = "SELECT * FROM states WHERE BINARY states.name LIKE 'N%' ORDER BY states.id ASC"
 cursor.execute(query)
     
 #display
