58886f0db0a073767c94fd2ae2f9219c7b008087
Vegas90 <amosmwoni@gmail.com>
2023-08-24 15:58:04 +0300

    M

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -6,8 +6,9 @@ db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], 
 cursor= db.cursor()
 
 #Execute the query to list states in ascending order by id
-query = "SELECT * FROM states WHERE states.name={} ORDER BY states.id ASC".format(argv[4])
-cursor.execute(query)
+query = "SELECT * FROM states WHERE states.name=%s ORDER BY states.id ASC"
+name=(argv[4],)
+cursor.execute(query,name)
     
 #display
 results= cursor.fetchall()
