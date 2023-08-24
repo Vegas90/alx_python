eec4af55b4045c4a85bff9ce64371ea3022a82ef
Vegas90 <amosmwoni@gmail.com>
2023-08-24 16:47:55 +0300

    M

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -6,7 +6,7 @@ db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], 
 cursor= db.cursor()
 
 #Execute the query to list states in ascending order by id
-query = "SELECT * FROM cities ORDER BY cities.id ASC"
+query = "SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id = states.id  ORDER BY cities.id ASC"
 # Put the parameter in a tuple 
 cursor.execute(query)
     
