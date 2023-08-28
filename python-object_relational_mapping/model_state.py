a1d2208ba1b9d1b376336b31b598f093f7a0f5e6
Vegas90 <amosmwoni@gmail.com>
2023-08-28 16:14:34 +0300

    M

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,18 +1,25 @@
-# Create a database connection
-from sqlalchemy import create_engine, Column, Integer, String, create_engine
+# Import necessary modules
+from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-# Create a database connection
-engine = create_engine('mysql://argv[1]:argv[2]@localhost:3306/argv[3]')
-#create base class
+
+# Create a database connection using command line arguments
+# The arguments are expected to be provided when the script is run
+# Example usage: python script.py username password database_name
+# argv[1] corresponds to username, argv[2] corresponds to password, argv[3] corresponds to database_name
+# Replace 'script.py' with the actual name of your script file
+engine = create_engine(f'mysql://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}')
+
+# Create a base class for declarative models
 Base = declarative_base()
-#defined mapped class
+
+# Define a mapped class named 'State'
 class State(Base):
-    #defined mapped class
-    __tablename__= 'states'
-    #defined mapped class
-    id= Column(Integer, primary_key=True, autoincrement=True)
-    #defined mapped class
-    name = Column(String(128),nullable=False)
-     
-# Import other mapped classes before create_all
+    # Specify the name of the table in the database
+    __tablename__ = 'states'
+    
+    # Define attributes for the 'states' table
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
+
+# Create the tables in the database
 Base.metadata.create_all(engine)
