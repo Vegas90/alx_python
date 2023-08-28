0357e6b1790aee9e821919f99bd278f5254ffc31
Vegas90 <amosmwoni@gmail.com>
2023-08-28 16:47:28 +0300

    M

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -8,15 +8,14 @@ engine = create_engine(f'mysql://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}')
 #Create a base class for declarative models
 Base = declarative_base()
 
-"""
-Define a mapped class named 'State'
-python3 -c 'print(__import__("my_module").MyClass.__doc__)"""
+#define class
+#mre class
 class State(Base):
     # Specify the name of the table in the database
     __tablename__ = 'states'
+    
     # Define attributes for the 'states' table
     id = Column(Integer, primary_key=True, autoincrement=True)
-    # Define a mapped class named 'State'
     name = Column(String(128), nullable=False)
 
 # Create the tables in the database
