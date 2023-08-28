9cfb85b7d2759f0e2e86419cfa3514cec0167a5f
Vegas90 <amosmwoni@gmail.com>
2023-08-28 16:52:35 +0300

    M

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,23 +1,23 @@
-# Specify the name of the table in the database
-from sqlalchemy import create_engine, Column, Integer, String, argv
+from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+import sys
 
 # Create a database connection using command line arguments
-engine = create_engine(f'mysql://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}')
+engine = create_engine(f'mysql://{sys.argv[1]}:{sys.argv[2]}@localhost:3306/{sys.argv[3]}')
 
-#Create a base class for declarative models
+# Create a base class for declarative models
 Base = declarative_base()
 
-#define class
-#mre class
 class State(Base):
-   """
+    """
     Represents a state in the 'states' table.
     
     Attributes:
         id (int): The primary key identifier for the state.
         name (str): The name of the state.
     """
+    
+    # Specify the name of the table in the database
     __tablename__ = 'states'
     
     # Define attributes for the 'states' table
@@ -25,4 +25,4 @@ class State(Base):
     name = Column(String(128), nullable=False)
 
 # Create the tables in the database
-Base.metadata.create_all(engine)
+Base
