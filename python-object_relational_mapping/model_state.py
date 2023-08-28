967f37283448e7325b70a7c41dbcc865569bab4a
Vegas90 <amosmwoni@gmail.com>
2023-08-28 16:19:11 +0300

    M

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -4,12 +4,14 @@ from sqlalchemy.ext.declarative import declarative_base
 engine = create_engine(f'mysql://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}')
 # Create a base class for declarative models
 Base = declarative_base()
+
 # Define a mapped class named 'State'
 class State(Base):
     # Specify the name of the table in the database
     __tablename__ = 'states'
     # Define attributes for the 'states' table
     id = Column(Integer, primary_key=True, autoincrement=True)
+    # Define a mapped class named 'State'
     name = Column(String(128), nullable=False)
 
 # Create the tables in the database
