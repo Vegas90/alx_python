9051ffc3f51a01dc26e8374c174c02d7a1036924
Vegas90 <amosmwoni@gmail.com>
2023-08-28 16:10:34 +0300

    M

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,18 +1,16 @@
 # Create a database connection
 from sqlalchemy import create_engine, Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
-
 # Create a database connection
 engine = create_engine('mysql://argv[1]:argv[2]@localhost:3306/argv[3]')
-
 #create base class
 Base = declarative_base()
-
 #defined mapped class
 class State(Base):
     __tablename__= 'states'
     #defined mapped class
     id= Column(Integer, primary_key=True, autoincrement=True)
+    #defined mapped class
     name = Column(String(128),nullable=False)
      
 # Import other mapped classes before create_all
