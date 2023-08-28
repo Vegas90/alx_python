daa2bd4fbbfece2b0d8795397bc1d1c37e0c7bd5
Vegas90 <amosmwoni@gmail.com>
2023-08-28 23:47:43 +0300

    M

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+""" 
+contains state class and ......
+"""
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+# Create a database connection using command line arguments
+engine = create_engine(f'mysql://{sys.argv[1]}:{sys.argv[2]}@localhost:3306/{sys.argv[3]}')
+ # Create a configured "Session" class
+Session = sessionmaker(bind=engine)
+# Create a Session
+session = Session()
+# Query the State table and order the results by id in ascending order
+states = session.query(State).order_by(State.id)
+# Print the results
+for state in states:
+    print(state.id, state.name)
+# Close the session
+session.close()
+
+
