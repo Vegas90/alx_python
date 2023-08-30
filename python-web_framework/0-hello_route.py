577bb764bab5ed3f8771185856773e69d9bf5ace
Vegas90 <amosmwoni@gmail.com>
2023-08-30 22:29:56 +0300

    M

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -2,9 +2,13 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/"strict_slashes=False)
+#create instance of this class
+@app.route("/",strict_slashes=False)
+#returns messages we want to display
 def hello():
+    #returns
     return "Hello HBNB!"
 
+#You must use the option strict_slashes=False in your route definition
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
