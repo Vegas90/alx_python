e840738ee7f8b15cb4ff6ad30430b28c7914199a
Vegas90 <amosmwoni@gmail.com>
2023-08-30 22:40:41 +0300

    M

---
+++ python-web_framework/1-hbnb_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+"""
+A script that starts a flas web application:
+Listening on 0.0.0.0 port 5000
+Routes: /display "Hello HBNB!"
+"""
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/",strict_slashes=False)
+def hello():
+   """
+   This function returns the specified string when the text in that directory
+   """
+   return "Hello HBNB!"
+
+@app.route("/hbnb",strict_slashes=False)
+def hellow():
+   """
+   This function returns the specified string when the text in that directory
+   """
+   return "HBNB"
+
+#You must use the option strict_slashes=False in your route definition
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
