fc4669c175157a54f555e3df8d3a3e183fa7495f
Vegas90 <amosmwoni@gmail.com>
2023-08-31 01:03:32 +0300

    M

--- python-web_framework/5-number_template.py
+++ python-web_framework/5-number_template.py
@@ -49,10 +49,10 @@ def num(n):
 
 #/number_template/<n>: display a HTML page only if n is an integer:
 @app.route("/number_template/<int:n>", strict_slashes=False)
-def num(n):
+def numc(n):
     """
-   number_template/<n>: display a HTML page only if n is an integer:
-   """
+    check by this number_template/<n>: display a HTML page only if n is an integer:
+    """
     n=n
     return render_template("5-number.html", n=n)
 
