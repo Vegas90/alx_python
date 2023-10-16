57b2a5c239fbdcd978e97925c2583940f17b73b2
Vegas90 <amosmwoni@gmail.com>
2023-10-16 12:10:19 +0300

    M

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,39 +1,21 @@
 import requests
 import sys
-
-def get_employee_info(employee_id):
-    # Retrieve employee details
-    employee_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    employee_response = requests.get(employee_url)
-    if employee_response.status_code != 200:
-        print(f"Error: Could not fetch employee details for ID {employee_id}")
-        return
-
-    employee_data = employee_response.json()
-    employee_name = employee_data['name']
-
-    # Retrieve employee's TODO list
-    todo_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
-    todo_response = requests.get(todo_url)
-    if todo_response.status_code != 200:
-        print(f"Error: Could not fetch TODO list for ID {employee_id}")
-        return
-
-    todo_data = todo_response.json()
-
-    # Calculate progress
-    total_tasks = len(todo_data)
-    completed_tasks = sum(1 for task in todo_data if task['completed'])
-
-    # Print employee TODO list progress
-    print(f"Employee {employee_name} is done with tasks ({completed_tasks}/{total_tasks}):")
-    for task in todo_data:
-        if task['completed']:
-            print(f"\t{task['title']}")
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <employee_id>")
-    else:
-        employee_id = int(sys.argv[1])
-        get_employee_info(employee_id)
+# Get the employee ID from the command line
+employee_id = int(sys.argv[1])
+# Get the employee's TODO list
+response = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos".format(employee_id))
+todos = response.json()
+# Get the employee's name
+response = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(employee_id))
+employee = response.json()
+employee_name = employee["name"]
+# Count the number of completed tasks
+completed_tasks = 0
+for todo in todos:
+    if todo["completed"]:
+        completed_tasks += 1
+# Print the employee's TODO list progress
+print("Employee {} is done with tasks({}/{}):".format(employee_name, completed_tasks, len(todos)))
+for todo in todos:
+    if todo["completed"]:
+        print("\t {}".format(todo["title"]))
